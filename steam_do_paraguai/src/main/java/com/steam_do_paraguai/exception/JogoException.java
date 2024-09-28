d6dbd5f007b5be699ad3d0d5197363c650c70172
João Victor Nicácio <joaovictornicaciosilva@gmail.com>
2024-09-28 20:30:48 -0300

    Cria exceção para Jogo

---
+++ steam_do_paraguai/src/main/java/com/steam_do_paraguai/exception/JogoException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.steam_do_paraguai.exception;
+
+/**
+ *
+ * @author joaov
+ */
+public class JogoException extends Exception{
+    public JogoException(String mensagem){
+        super(mensagem);
+    }
+}
